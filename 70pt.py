fd02c24477d172364001a555430237294a9a7116
Dalton125 <daltong10@gmail.com>
2014-11-10 15:10:12 -0600

    70 point

--- 70pt.py
+++ 70pt.py
@@ -34,7 +34,7 @@ class MyApp:
 					
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
-
+		
 		  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
@@ -45,12 +45,20 @@ class MyApp:
                 # "global" makes sure that we can access our oval and our drawpad
 		global oval
 		global drawpad
-                x1,y1,x2,y2 = drawpad.coords(player)
-		global targetx1, targety1, targetx2, targety2
-
-
+		global player 
+		drawpad.move(player,0,-20)
+		global target 
+                global targetx1, targety1, targetx2, targety2
+                px1,py1,px2,py2 = drawpad.coords(player)
+	
+	
 		# Ensure that we are doing our collision detection
 		# After we move our object!
+		
+	        if (px1 > targetx1 and px2 < targetx2) and (py1 > targety1 and py2 < targety2):
+	           drawpad.itemconfig(target, fill="red")
+	        else:
+	           drawpad.itemconfig(target, fill="blue")
 	
 	
 		
